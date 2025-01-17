60b1525aa2dcee4e77af6f87b023e8738d698658
Tamara Manaseer <tmanaseer@EpyaSolutions.com>
2025-01-17 08:21:42 +0300

    First Shot

---
+++ Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Appointment
+{
+    public Appointment()
+    {
+    }
+
+    public Guid Id { get; set; }
+    public Guid SlotId { get; set; }
+    public Guid PatientId { get; set; }
+    public DateTime ReservedAt { get; set; }
+}
---
+++ Seeder.cs
@@ -0,0 +1,12 @@
+﻿public static class Seeder
+{
+    public static List<Doctor> GetDoctorAvailabilities()
+    {
+        return new List<Doctor>
+        {
+            new Doctor { Id = 1, DoctorName = "Dr. Smith", AvailableDate = DateTime.Now.AddDays(1) },
+            new DoctorAvailability { Id = 2, DoctorName = "Dr. Johnson", AvailableDate = DateTime.Now.AddDays(2) },
+            new DoctorAvailability { Id = 3, DoctorName = "Dr. Brown", AvailableDate = DateTime.Now.AddDays(3) }
+        };
+    }
+}
